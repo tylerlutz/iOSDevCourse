e062b4ac7d17cbb0715ee7a235102c8bdb2ec706
Tyler Lutz <tyler@tylerlutz.com>
2016-10-19 21:38:32 -0400

    Starting MiraclePill Project

---
+++ MiraclePill/MiraclePill/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MiraclePill
+//
+//  Created by Justen Lutz on 10/19/16.
+//  Copyright © 2016 Tyler Lutz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
