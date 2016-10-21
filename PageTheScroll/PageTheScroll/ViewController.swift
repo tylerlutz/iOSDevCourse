5cf2bc0a8cc70c241bea295637d1aae83db91762
Tyler Lutz <tyler@tylerlutz.com>
2016-10-20 21:59:21 -0400

    Completed ScrollView Project.

--- PageTheScroll/PageTheScroll/ViewController.swift
+++ PageTheScroll/PageTheScroll/ViewController.swift
@@ -10,17 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var scrollView: UIScrollView!
     var images = [UIImageView]()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        var contentWidth: CGFloat = 0.0
+        
+        let scrollWidth = scrollView.frame.size.width
         
         for x in 0...2{
             let image = UIImage(named: "icon\(x).png")
-            images.append(UIImageView(image: image))
+            let imageView = UIImageView(image:image)
+            images.append(imageView)
+            
+            var newX: CGFloat = 0.0
+            
+            newX = scrollWidth / 2 + scrollWidth * CGFloat(x)
+            
+            contentWidth += newX
+            
+            scrollView.addSubview(imageView)
+            
+            imageView.frame = CGRect(x: newX - 75, y: scrollView.frame.size.height / 2 - 75, width: 150, height: 150)
+            
+            
         }
         
-        
+        scrollView.clipsToBounds = false
+        scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
     }
 
 
