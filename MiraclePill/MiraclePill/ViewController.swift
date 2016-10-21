943869bd97df807043041da34bd9cf85040fc959
Tyler Lutz <tyler@tylerlutz.com>
2016-10-20 21:15:52 -0400

    Completed MiraclePill App

--- MiraclePill/MiraclePill/ViewController.swift
+++ MiraclePill/MiraclePill/ViewController.swift
@@ -8,10 +8,84 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var pillImg: UIImageView!
+    @IBOutlet weak var miracleLbl: UILabel!
+    @IBOutlet weak var priceLbl: UILabel!
+    @IBOutlet weak var dividerImg: UIView!
+    @IBOutlet weak var statePicker: UIPickerView!
+    @IBOutlet weak var statePickerBtn: UIButton!
+    @IBOutlet weak var successImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var nameTxt: UITextField!
+    @IBOutlet weak var addressLbl: UILabel!
+    @IBOutlet weak var addressTxt: UITextField!
+    @IBOutlet weak var cityLbl: UILabel!
+    @IBOutlet weak var cityTxt: UITextField!
+    @IBOutlet weak var stateLbl: UILabel!
+    @IBOutlet weak var buyNowBtn: UIButton!
+    
+    let states = [ "AK - Alaska",
+                  "AL - Alabama",
+                  "AR - Arkansas",
+                  "AS - American Samoa",
+                  "AZ - Arizona",
+                  "CA - California",
+                  "CO - Colorado",
+                  "CT - Connecticut",
+                  "DC - District of Columbia",
+                  "DE - Delaware",
+                  "FL - Florida",
+                  "GA - Georgia",
+                  "GU - Guam",
+                  "HI - Hawaii",
+                  "IA - Iowa",
+                  "ID - Idaho",
+                  "IL - Illinois",
+                  "IN - Indiana",
+                  "KS - Kansas",
+                  "KY - Kentucky",
+                  "LA - Louisiana",
+                  "MA - Massachusetts",
+                  "MD - Maryland",
+                  "ME - Maine",
+                  "MI - Michigan",
+                  "MN - Minnesota",
+                  "MO - Missouri",
+                  "MS - Mississippi",
+                  "MT - Montana",
+                  "NC - North Carolina",
+                  "ND - North Dakota",
+                  "NE - Nebraska",
+                  "NH - New Hampshire",
+                  "NJ - New Jersey",
+                  "NM - New Mexico",
+                  "NV - Nevada",
+                  "NY - New York",
+                  "OH - Ohio",
+                  "OK - Oklahoma",
+                  "OR - Oregon",
+                  "PA - Pennsylvania",
+                  "PR - Puerto Rico",
+                  "RI - Rhode Island",
+                  "SC - South Carolina",
+                  "SD - South Dakota",
+                  "TN - Tennessee",
+                  "TX - Texas",
+                  "UT - Utah",
+                  "VA - Virginia",
+                  "VI - Virgin Islands",
+                  "VT - Vermont",
+                  "WA - Washington",
+                  "WI - Wisconsin",
+                  "WV - West Virginia", 
+                  "WY - Wyoming"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        statePicker.dataSource = self
+        statePicker.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -19,6 +93,43 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func stateBtnPressed(_ sender: AnyObject) {
+        statePicker.isHidden = false
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        statePickerBtn.setTitle(states[row], for: UIControlState.normal)
+        statePicker.isHidden = true
+    }
 
+    @IBAction func buyNowBtnPressed(_ sender: AnyObject) {
+        pillImg.isHidden = true
+        miracleLbl.isHidden = true
+        priceLbl.isHidden = true
+        dividerImg.isHidden = true
+        nameLbl.isHidden = true
+        nameTxt.isHidden = true
+        addressLbl.isHidden = true
+        addressTxt.isHidden = true
+        cityLbl.isHidden = true
+        cityTxt.isHidden = true
+        stateLbl.isHidden = true
+        statePickerBtn.isHidden = true
+        statePicker.isHidden = true
+        buyNowBtn.isHidden = true
+        successImg.isHidden = false
+    }
 }
 
